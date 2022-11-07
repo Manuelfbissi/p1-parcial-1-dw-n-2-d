b1e015fce0eb37052921326d612e4b753b27f054
Manuel Bissi <manuel.fernandez@davinci.edu.ar>
2022-11-07 15:38:53 -0300

    codigo unico

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 'use strict';
 let nombreDisco;
 let nombreAutor;
-let codigoDisco;
+let codigoDisco=[];
 let nombrePista;
 let duracionPista;
 let contadorDiscos=1;
+let disco;
+let pista;
+let color="red";
+
 // Discos:
 let discos = [];
 let pistas=[];
 
 
+             
 const Cargar = () => {
  do {
 pedirDisco()
+
  //guardar disco.
  
  
@@ -24,9 +30,9 @@ pedirPista()
 
 
 } while (confirm("¿Desea ingresar otro disco?"));
-console.log (contadorDiscos++)
+//console.log (contadorDiscos++)
 console.log(discos)
-console.log(pistas)
+
 
 };
 
@@ -49,19 +55,23 @@ function pedirDisco (){
     
      do {
         codigoDisco=Number (prompt("Ingrese codigo del disco"))
-        if (codigoDisco===""||(!(codigoDisco>=1 && codigoDisco<=999)) ) {
-            alert ("El valor debe ser mayor a 1 y menor a 999")
-            
+        if ((codigoDisco==="") || (!(codigoDisco>=1 && codigoDisco<=999) ) || (discos.length === codigoDisco)) {
+            alert ("El valor debe ser mayor a 1 y menor a 999 o bien el codigo ya fue usado")
+             
             }
-        } while (!(codigoDisco>=1 && codigoDisco<=999) || (codigoDisco===""));
-        
+            
+        } while  ((codigoDisco==="") || (!(codigoDisco>=1 && codigoDisco<=999) ) || (discos.length === codigoDisco));
+
         let disco={
             nombre:nombreDisco,
             autor:nombreAutor,
             codigo:codigoDisco,
-            pista:pistas,
             }
             discos.push(disco)
+           
+        
+        return discos;
+           
 
 }
 
@@ -79,22 +89,29 @@ function pedirPista(){
             if ((duracionPista==="") || (isNaN (duracionPista))) {
                 alert ("Ingrese la duracion de la pista expresada en segundos")
                 }
+
+          
         } while ((duracionPista==="") || (isNaN (duracionPista)));
-        
+
         let pista={
             nombre:nombrePista,
             duracion:duracionPista,
         }
         pistas.push(pista)
+          
         
     
     } while (confirm("¿Desea cargar otra pista?"));
-    return pistas;
-    return discos;
+    return pistas; 
 
-        
+
+
        
 }
+
+ 
+ 
+
 
 // Función Mostrar:
 function Mostrar(){
@@ -111,16 +128,19 @@ function Mostrar(){
     console.log (item);
    }
       for (let i = 0; i < discos.length; i++){
-    html += `<strong> Nombre del disco: </strong> <li> ${discos[i].nombre}</li>`
-    html += `<strong> Autor: </strong> <li> ${discos[i].autor}</li>`
-    html += `<strong> Codigo unico del disco: </strong> <li> ${discos[i].codigo}</li>`
-
+    html += `<strong> Nombre del disco: </strong> <li> ${discos[i].nombre}</li>`;
+    html += `<strong> Autor: </strong> <li> ${discos[i].autor}</li>`;
+    html += `<strong> Codigo unico del disco: </strong> <li> ${discos[i].codigo}</li>`;
+      
     
+    for (let j = 0; j < pistas.length; j++){
+    html += `<strong> Nombre de la pista: </strong> <li> ${pistas[j].nombre}</li>`;
+    html += `<strong> duracion de la pista: </strong style = "color: ${color}"> <li> ${pistas[j].duracion}</li>`;
 }
- for (let j = 0; j < pistas.length; j++){
-    html += `<strong> Nombre de la pista: </strong> <li> ${pistas[j].nombre}</li>`
-    html += `<strong> Duracion de la pista: </strong> <li> ${pistas[j].duracion}</li>`
-    }
+
+}
+    
+    
  html+= "</ul>";
      
    
